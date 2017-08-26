483be4099571a6c874de2c4f030d3bd4787137c2
stkim1 <stkim1@colorfulglue.com>
2017-08-27 07:24:29 +0900

    indent, format

--- blocksources/helpers.go
+++ blocksources/helpers.go
@@ -1,8 +1,8 @@
 package blocksources
 
 import (
-	"fmt"
-	"github.com/Redundancy/go-sync/patcher"
+    "fmt"
+    "github.com/Redundancy/go-sync/patcher"
 )
 
 // errorWatcher is a small helper object
@@ -10,115 +10,115 @@ import (
 // so w.sendIfSet() <- w.Err() is always safe in a select statement
 // even if there is no error set
 type errorWatcher struct {
-	errorChannel chan error
-	lastError    error
+    errorChannel chan error
+    lastError    error
 }
 
 func (w *errorWatcher) setError(e error) {
-	if w.lastError != nil {
-		panic("cannot set a new error when one is already set!")
-	}
-	w.lastError = e
+    if w.lastError != nil {
+        panic("cannot set a new error when one is already set!")
+    }
+    w.lastError = e
 }
 
 func (w *errorWatcher) clear() {
-	w.lastError = nil
+    w.lastError = nil
 }
 
 func (w *errorWatcher) Err() error {
-	return w.lastError
+    return w.lastError
 }
 
 func (w *errorWatcher) sendIfSet() chan<- error {
-	if w.lastError != nil {
-		return w.errorChannel
-	} else {
-		return nil
-	}
+    if w.lastError != nil {
+        return w.errorChannel
+    } else {
+        return nil
+    }
 }
 
 type pendingResponseHelper struct {
-	responseChannel chan patcher.BlockReponse
-	pendingResponse *patcher.BlockReponse
+    responseChannel chan patcher.BlockReponse
+    pendingResponse *patcher.BlockReponse
 }
 
 func (w *pendingResponseHelper) setResponse(r *patcher.BlockReponse) {
-	if w.pendingResponse != nil {
-		p := fmt.Sprintf("Setting a response when one is already set! Had startblock %v, got %v", r.StartBlock, w.pendingResponse.StartBlock)
-		panic(p)
-	}
-	w.pendingResponse = r
+    if w.pendingResponse != nil {
+        p := fmt.Sprintf("Setting a response when one is already set! Had startblock %v, got %v", r.StartBlock, w.pendingResponse.StartBlock)
+        panic(p)
+    }
+    w.pendingResponse = r
 }
 
 func (w *pendingResponseHelper) clear() {
-	w.pendingResponse = nil
+    w.pendingResponse = nil
 }
 
 func (w *pendingResponseHelper) Response() patcher.BlockReponse {
-	if w.pendingResponse == nil {
-		return patcher.BlockReponse{}
-	}
-	return *w.pendingResponse
+    if w.pendingResponse == nil {
+        return patcher.BlockReponse{}
+    }
+    return *w.pendingResponse
 }
 
 func (w *pendingResponseHelper) sendIfPending() chan<- patcher.BlockReponse {
-	if w.pendingResponse != nil {
-		return w.responseChannel
-	} else {
-		return nil
-	}
+    if w.pendingResponse != nil {
+        return w.responseChannel
+    } else {
+        return nil
+    }
 
 }
 
 type UintSlice []uint
 
 func (r UintSlice) Len() int {
-	return len(r)
+    return len(r)
 }
 
 func (r UintSlice) Swap(i, j int) {
-	r[i], r[j] = r[j], r[i]
+    r[i], r[j] = r[j], r[i]
 }
 
 func (r UintSlice) Less(i, j int) bool {
-	return r[i] < r[j]
+    return r[i] < r[j]
 }
 
 type asyncResult struct {
-	startBlockID uint
-	endBlockID   uint
-	data         []byte
-	err          error
+    startBlockID uint
+    endBlockID   uint
+    data         []byte
+    err          error
 }
 
 type QueuedRequest struct {
-	StartBlockID uint
-	EndBlockID   uint
+    StartBlockID uint
+    EndBlockID   uint
 }
 
 type QueuedRequestList []QueuedRequest
 
 func (r QueuedRequestList) Len() int {
-	return len(r)
+    return len(r)
 }
 
 func (r QueuedRequestList) Swap(i, j int) {
-	r[i], r[j] = r[j], r[i]
+    r[i], r[j] = r[j], r[i]
 }
 
 func (r QueuedRequestList) Less(i, j int) bool {
-	return r[i].StartBlockID < r[j].StartBlockID
+    return r[i].StartBlockID < r[j].StartBlockID
 }
 
 func MakeNullFixedSizeResolver(blockSize uint64) BlockSourceOffsetResolver {
-	return &FixedSizeBlockResolver{
-		BlockSize: blockSize,
-	}
+    return &FixedSizeBlockResolver{
+        BlockSize: blockSize,
+    }
 }
 
 func MakeFileSizedBlockResolver(blockSize uint64, filesize int64) BlockSourceOffsetResolver {
-	return &FixedSizeBlockResolver{
-		BlockSize: blockSize,
-		FileSize:  filesize,
-	}
+    return &FixedSizeBlockResolver{
+        BlockSize: blockSize,
+        FileSize:  filesize,
+    }
 }
--- blocksources/readseeker_blocksource_test.go
+++ blocksources/readseeker_blocksource_test.go
@@ -1,42 +1,43 @@
 package blocksources
 
 import (
-	"bytes"
-	"github.com/Redundancy/go-sync/patcher"
-	"testing"
+    "bytes"
+    "testing"
+
+    "github.com/Redundancy/go-sync/patcher"
 )
 
 const STRING_DATA = "abcdefghijklmnopqrst"
 
 var BYTE_BLOCK_SOURCE = NewReadSeekerBlockSource(
-	bytes.NewReader(
-		[]byte(STRING_DATA),
-	),
-	MakeNullFixedSizeResolver(4),
+    bytes.NewReader(
+        []byte(STRING_DATA),
+    ),
+    MakeNullFixedSizeResolver(4),
 )
 
 func TestReadFirstBlock(t *testing.T) {
-	BYTE_BLOCK_SOURCE.RequestBlocks(
-		patcher.MissingBlockSpan{
-			BlockSize:  4,
-			StartBlock: 0,
-			EndBlock:   0,
-		},
-	)
+    BYTE_BLOCK_SOURCE.RequestBlocks(
+        patcher.MissingBlockSpan{
+            BlockSize:  4,
+            StartBlock: 0,
+            EndBlock:   0,
+        },
+    )
 
-	result := <-BYTE_BLOCK_SOURCE.GetResultChannel()
+    result := <-BYTE_BLOCK_SOURCE.GetResultChannel()
 
-	if result.StartBlock != 0 {
-		t.Errorf("Wrong start block: %v", result.StartBlock)
-	}
+    if result.StartBlock != 0 {
+        t.Errorf("Wrong start block: %v", result.StartBlock)
+    }
 
-	EXPECTED := STRING_DATA[:4]
+    EXPECTED := STRING_DATA[:4]
 
-	if bytes.Compare(result.Data, []byte(EXPECTED)) != 0 {
-		t.Errorf(
-			"Unexpected result data: \"%v\" expected: \"%v\"",
-			string(result.Data),
-			EXPECTED,
-		)
-	}
+    if bytes.Compare(result.Data, []byte(EXPECTED)) != 0 {
+        t.Errorf(
+            "Unexpected result data: \"%v\" expected: \"%v\"",
+            string(result.Data),
+            EXPECTED,
+        )
+    }
 }
