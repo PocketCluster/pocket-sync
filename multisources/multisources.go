74204ded60eb55612018984da6af977e1d4f1729
stkim1 <stkim1@colorfulglue.com>
2017-08-27 17:54:35 +0900

    multi block sources

---
+++ multisources/multisources.go
@@ -0,0 +1,18 @@
+package multisources
+
+import (
+    "sort"
+
+    log "github.com/Sirupsen/logrus"
+    "github.com/pkg/errors"
+    "github.com/Redundancy/go-sync/patcher"
+)
+
+
+/*
+ * MultiSourcesBase takes
+ */
+
+type MultiSourcesBase struct {
+
+}
