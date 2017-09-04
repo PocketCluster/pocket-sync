aa0f8e99dde28d96f4575467315ce2d7ffe7c3ea
stkim1 <stkim1@colorfulglue.com>
2017-09-04 22:08:03 +0900

    filechecksum with merkle tree added

---
+++ filechecksum/filechecksum_merkle.go
@@ -0,0 +1,132 @@
+package filechecksum
+
+import (
+    "io"
+
+    "github.com/Redundancy/go-sync/chunks"
+    "github.com/Redundancy/go-sync/merkle"
+)
+
+func (check *FileChecksumGenerator) BuildSequentialAndRootChecksum(inputFile io.Reader, output io.Writer) ([]byte, error) {
+    for chunkResult := range check.startBuildChecksum(inputFile, 64) {
+        if chunkResult.Err != nil {
+            return nil, chunkResult.Err
+        } else if chunkResult.Filechecksum != nil {
+            return chunkResult.Filechecksum, nil
+        }
+
+        for _, chunk := range chunkResult.Checksums {
+            output.Write(chunk.WeakChecksum)
+            output.Write(chunk.StrongChecksum)
+        }
+    }
+
+    return nil, nil
+}
+
+func (check *FileChecksumGenerator) startBuildChecksum(
+    inputFile       io.Reader,
+    blocksPerResult uint,
+) <-chan ChecksumResults {
+    resultChan := make(chan ChecksumResults)
+    go check.buildSeqAndRootChecksum(inputFile, resultChan, blocksPerResult)
+    return resultChan
+}
+
+func (check *FileChecksumGenerator) buildSeqAndRootChecksum(
+    inputFile       io.Reader,
+    resultChan      chan ChecksumResults,
+    blocksPerResult uint,
+) {
+
+    defer close(resultChan)
+
+    var (
+        blockSize   = int64(check.BlockSize())
+        buffer      = make([]byte, check.BlockSize())
+        results     = make([]chunks.ChunkChecksum, 0, blocksPerResult)
+        sChksum     = make([][]byte, 0, 1)
+
+        // these hashes are generated and reset to make it clean
+        // As these are for one-time use only, throw away as soon as you're done
+        rollingHash = check.GetWeakRollingHash()
+        strongHash  = check.GetStrongHash()
+    )
+
+    i := uint(0)
+    for {
+        n, err := io.ReadFull(inputFile, buffer)
+        // loop only breaks when there is nothing read (so partial reads gets handled)
+        if n == 0 {
+            break
+        }
+
+        var (
+            section = buffer[:n]
+            weakChecksumValue   = make([]byte, rollingHash.Size())
+            strongChecksumValue = make([]byte, 0, strongHash.Size())
+        )
+
+        // As hashes, the assumption is that they never error.
+        // Additionally, we assume that the only reason not to write a full block would be reaching the end of the file.
+        rollingHash.SetBlock(section)
+        strongHash.Write(section)
+
+        // get checksum
+        rollingHash.GetSum(weakChecksumValue)
+        strongChecksumValue = strongHash.Sum(strongChecksumValue)
+
+        // append to result
+        results = append(
+            results,
+            chunks.ChunkChecksum{
+                ChunkOffset:    i,
+                Size:           blockSize,
+                WeakChecksum:   weakChecksumValue,
+                StrongChecksum: strongChecksumValue,
+            })
+
+        // save strong checksum for root hash
+        sChksum = append(sChksum, strongChecksumValue)
+
+        // dispatch collected result
+        if len(results) == cap(results) {
+            resultChan <- ChecksumResults{
+                Checksums: results,
+            }
+            results = make([]chunks.ChunkChecksum, 0, blocksPerResult)
+        }
+
+        // advance iterator
+        i++
+
+        // Reset the strong (we don't reset rollingsum as it needs previous data)
+        strongHash.Reset()
+
+        // stops at io.EOF, io.ErrUnexpectedEOF
+        if err != nil {
+            break
+        }
+    }
+
+    // end
+    if len(results) > 0 {
+        resultChan <- ChecksumResults{
+            Checksums: results,
+        }
+    }
+
+    // make root hash
+    rcs, err := merkle.SimpleHashFromHashes(sChksum)
+    if err != nil {
+        resultChan <- ChecksumResults{
+            Err: err,
+        }
+    } else {
+        resultChan <- ChecksumResults{
+            Filechecksum: rcs,
+        }
+    }
+
+    return
+}
---
+++ filechecksum/filechecksum_merkle_test.go
@@ -0,0 +1,253 @@
+package filechecksum
+
+import (
+    "bytes"
+    "io"
+    "testing"
+
+    "github.com/Redundancy/go-sync/chunks"
+    "github.com/Redundancy/go-sync/util/readers"
+)
+
+func Test_BuildSeqRootChecksums_EndsWithFilechecksum(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        emptybuffer = bytes.NewBuffer(make([]byte, BLOCK_COUNT * BLOCKSIZE))
+        checksum    = NewFileChecksumGenerator(BLOCKSIZE)
+        lastResult  = ChecksumResults{}
+    )
+
+    for lastResult = range checksum.startBuildChecksum(emptybuffer, 10) {
+    }
+
+    if lastResult.Checksums != nil {
+        t.Errorf("Last result had checksums: %#v", lastResult)
+    }
+
+    if lastResult.Filechecksum == nil {
+        t.Errorf("Last result did not contain the filechecksum: %#v", lastResult)
+    } else {
+        t.Logf("FileChecksum %v", lastResult.Filechecksum)
+    }
+}
+
+func Test_BuildSeqRootChecksums_ReturnsCorrectChecksumCount(t *testing.T) {
+    const(
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        emptybuffer = bytes.NewBuffer(make([]byte, BLOCK_COUNT * BLOCKSIZE))
+        checksum    = NewFileChecksumGenerator(BLOCKSIZE)
+        resultCount = 0
+    )
+
+    for r := range checksum.startBuildChecksum(emptybuffer, 10) {
+        resultCount += len(r.Checksums)
+    }
+
+    if resultCount != BLOCK_COUNT {
+        t.Errorf("Unexpected block count returned: %v", resultCount)
+    }
+}
+
+func Test_BuildSeqRootChecksums_ContainsHashes(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        emptybuffer = bytes.NewBuffer(make([]byte, BLOCK_COUNT * BLOCKSIZE))
+        checksum    = NewFileChecksumGenerator(BLOCKSIZE)
+    )
+
+    for r := range checksum.startBuildChecksum(emptybuffer, 10) {
+        for _, r2 := range r.Checksums {
+            if len(r2.WeakChecksum) != checksum.GetWeakRollingHash().Size() {
+                t.Fatalf(
+                    "Wrong length weak checksum: %v vs %v",
+                    len(r2.WeakChecksum),
+                    checksum.GetWeakRollingHash().Size(),
+                )
+            }
+
+            if len(r2.StrongChecksum) != checksum.GetStrongHash().Size() {
+                t.Fatalf(
+                    "Wrong length strong checksum: %v vs %v",
+                    len(r2.StrongChecksum),
+                    checksum.GetStrongHash().Size(),
+                )
+            }
+
+        }
+    }
+}
+
+func Test_BuildSeqRootChecksums_ExpectedLength(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        emptybuffer = bytes.NewBuffer(make([]byte, BLOCK_COUNT * BLOCKSIZE))
+        output      = bytes.NewBuffer(nil)
+        checksum    = NewFileChecksumGenerator(BLOCKSIZE)
+
+        // output length is expected to be 20 blocks
+        expectedLength = (BLOCK_COUNT * checksum.GetStrongHash().Size()) +
+                         (BLOCK_COUNT * checksum.GetWeakRollingHash().Size())
+    )
+
+    _, err := checksum.BuildSequentialAndRootChecksum(emptybuffer, output)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if output.Len() != expectedLength {
+        t.Errorf(
+            "output length (%v) did not match expected length (%v)",
+            output.Len(),
+            expectedLength,
+        )
+    }
+}
+
+func Test_BuildSeqRootChecksums_ExpectedLengthWithPartialBlockAtEnd(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        FULL_BLOCK_COUNT = 20
+        BLOCK_COUNT = FULL_BLOCK_COUNT + 1
+    )
+    var (
+        emptybuffer = bytes.NewBuffer(make([]byte, FULL_BLOCK_COUNT*BLOCKSIZE+50))
+        output      = bytes.NewBuffer(nil)
+        checksum    = NewFileChecksumGenerator(BLOCKSIZE)
+
+        // output length is expected to be 20 blocks
+        expectedLength = (BLOCK_COUNT * checksum.GetStrongHash().Size()) +
+                         (BLOCK_COUNT * checksum.GetWeakRollingHash().Size())
+    )
+
+    _, err := checksum.BuildSequentialAndRootChecksum(emptybuffer, output)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if output.Len() != expectedLength {
+        t.Errorf(
+            "output length (%v) did not match expected length (%v)",
+            output.Len(),
+            expectedLength,
+        )
+    }
+}
+
+// Each of the data blocks is the same, so the checksums for the blocks should be the same
+func Test_BuildSeqRootChecksums_BlocksTheSame(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        checksum = NewFileChecksumGenerator(BLOCKSIZE)
+        output = bytes.NewBuffer(nil)
+    )
+
+    _, err := checksum.BuildSequentialAndRootChecksum(
+        readers.OneReader(BLOCKSIZE*BLOCK_COUNT),
+        output,
+    )
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    weakSize, strongSize := checksum.GetChecksumSizes()
+
+    if output.Len() != BLOCK_COUNT*(strongSize+weakSize) {
+        t.Errorf(
+            "Unexpected output length: %v, expected %v",
+            output.Len(),
+            BLOCK_COUNT*(strongSize+weakSize),
+        )
+    }
+
+    results, err := chunks.LoadChecksumsFromReader(output, weakSize, strongSize)
+
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if len(results) != BLOCK_COUNT {
+        t.Fatalf("Results too short! %v", len(results))
+    }
+
+    first := results[0]
+
+    for i, chk := range results {
+        if chk.ChunkOffset != uint(i) {
+            t.Errorf("Unexpected offset %v on chunk %v", chk.ChunkOffset, i)
+        }
+        if !first.Match(chk) {
+            t.Fatalf("Chunks have different checksums on %v", i)
+        }
+    }
+}
+
+func Test_BuildSeqRootChecksums_PrependedBlocks(t *testing.T) {
+    const (
+        BLOCKSIZE = 100
+        BLOCK_COUNT = 20
+    )
+    var (
+        checksum = NewFileChecksumGenerator(BLOCKSIZE)
+
+        file1 = io.LimitReader(
+            readers.NewNonRepeatingSequence(0),
+            BLOCKSIZE*BLOCK_COUNT,
+        )
+
+        file2 = io.LimitReader(
+            io.MultiReader(
+                readers.OneReader(BLOCKSIZE), // Off by one block
+                readers.NewNonRepeatingSequence(0),
+            ),
+            BLOCKSIZE*BLOCK_COUNT,
+        )
+    )
+
+    output1 := bytes.NewBuffer(nil)
+    chksum1, _ := checksum.BuildSequentialAndRootChecksum(file1, output1)
+
+    output2 := bytes.NewBuffer(nil)
+    chksum2, _ := checksum.BuildSequentialAndRootChecksum(file2, output2)
+
+    if bytes.Compare(chksum1, chksum2) == 0 {
+        t.Fatal("Checksums should be different")
+    }
+
+    weaksize, strongSize := checksum.GetChecksumSizes()
+    sums1, _ := chunks.LoadChecksumsFromReader(output1, weaksize, strongSize)
+    sums2, _ := chunks.LoadChecksumsFromReader(output2, weaksize, strongSize)
+
+    if len(sums1) != len(sums2) {
+        t.Fatalf("Checksum lengths differ %v vs %v", len(sums1), len(sums2))
+    }
+
+    if sums1[0].Match(sums2[0]) {
+        t.Error("Chunk sums1[0] should differ from sums2[0]")
+    }
+
+    for i, _ := range sums2 {
+        if i == 0 {
+            continue
+        }
+
+        if !sums1[i-1].Match(sums2[i]) {
+            t.Errorf("Chunk sums1[%v] equal sums2[%v]", i-1, i)
+        }
+
+    }
+}
